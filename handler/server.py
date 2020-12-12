a3d5234f040d0f0538039a9e9a1e52c76379fa09
Sylvan Le Deunff <sylvan.ledeunff@orange.com>
2020-12-12 11:44:03 +0100

    fix certificate use by authz handler

--- handler/server.py
+++ handler/server.py
@@ -1,10 +1,19 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return "hello"
+@app.route('/', methods=['POST'])
+def handle_authz():
+    print(request.json)
+    print('kind', request.json['kind'])
+    print('api_version', request.json['apiVersion'])
+    return {
+        "apiVersion": "authorization.k8s.io/v1beta1",
+        "kind": "SubjectAccessReview",
+        "status": {
+            "allowed": True
+        }
+    }
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8005, ssl_context=('../pki_v2/ca.crt', '../pki_v2/ca.key'), debug=True)
+    app.run(host='0.0.0.0', port=8005, ssl_context=('../pki/moon-pdp.crt', '../pki/moon-pdp.key'), debug=True)
